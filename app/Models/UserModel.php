7824a803c32ab1a757afaee3aef0c82d6547420f
ingvegambv <ingjulianvega1@gmail.com>
2026-03-01 23:13:26 -0600

    corregimos el error del timestap

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -21,7 +21,7 @@ class UserModel extends Model
     protected array $castHandlers = [];
 
     // Dates
-    protected $useTimestamps = true;
+    protected $useTimestamps = false;
     protected $dateFormat    = 'datetime';
     protected $createdField  = 'created_at';
     protected $updatedField  = 'updated_at';
